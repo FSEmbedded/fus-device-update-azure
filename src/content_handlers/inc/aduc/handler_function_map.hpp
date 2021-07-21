8a521063d4ae4f57b64c3ecb78b6099138d4745f
Matthias Schneider <schneider@fs-net.de>
2021-07-21 13:47:53 +0200

    Add fsupdate content handler
    This adds a new update handler to adu agent. There are a few differences
    when compared to the swupdate handler which fsupdate is based on.
    
    Fsupdate hast to distinguish between ff and af files, these selectors
    have been added to:
    - Install
    - IsInstalled
    
    The additional value is stored as a member variable of the handler
    object, and gets initialized when the object is created. To do this,
    there is a new constructor as well as a new call.
    
    The cancel Action was changed to noop, because there is no way to cancel
    FS-Update

--- src/content_handlers/inc/aduc/handler_function_map.hpp
+++ src/content_handlers/inc/aduc/handler_function_map.hpp
@@ -10,28 +10,13 @@
 #include <functional>
 #include <stdio.h>
 
-#if ADUC_SWUPDATE_HANDLER
-
+#if ADUC_FSUPDATE_HANDLER
 #    if !ADUC_SIMULATOR_MODE
-#        include "aduc/swupdate_handler.hpp"
-#    else
-#        include "aduc/swupdate_simulator_handler.hpp"
+#        include "aduc/fsupdate_handler.hpp"
 #    endif
-
-#endif
-
-#if ADUC_APT_HANDLER
-
-#    if !ADUC_SIMULATOR_MODE
-#        include "aduc/apt_handler.hpp"
-#    else
-#        include "aduc/apt_simulator_handler.hpp"
-#    endif
-
 #endif
 
 typedef std::function<std::unique_ptr<ContentHandler>(const ContentHandlerCreateData&)> CreateFuncType;
-
 typedef struct tagTypeFuncMap
 {
     const char* UpdateType;
@@ -58,6 +43,9 @@ const TypeFuncMap handlerCreateFuncs[] = {
 #endif
 #if ADUC_SWUPDATE_HANDLER
     FUNCMAPENTRY(microsoft, swupdate),
+#endif
+#if ADUC_FSUPDATE_HANDLER
+    FUNCMAPENTRY(fus, fsupdate),
 #endif
 };
 
